1062a36dd309a865b380898633f391d61bc822bf
AnthonyClausing <AnthonyClausing@gmail.com>
2021-03-07 21:42:25 -0500

    seeds

---
+++ db/seeds/user.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('user').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('user').insert([
+        {id: 1, company_id: 1, email:"owner@besttest.com", password_digest: "test123", first_name: "OwnerOf", last_name: "BestTest", phone: "347-555-1234"},
+        {id: 2, company_id: 2, email: "owner@targettesters.com", password_digest: "testtest", first_name: "OwnerOf", last_name: "TargetTesters", phone: "347-555-2345"},
+        {id: 3, company_id: 1, email: "manager@besttest.com", password_digest: "manager123", first_name: "ManagerOf", last_name: "BestTest", phone: "347-555-3456"},
+        {id: 4, company_id: 1, email: "employee@besttest.com", password_digest: "employee123", first_name: "EmployeeOf", last_name: "BestTest", phone: "347-555-4567"},
+      ]);
+    });
+};
