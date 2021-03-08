c0064d81feb770d4c6f8b288473d8b77ac322aee
AnthonyClausing <AnthonyClausing@gmail.com>
2021-03-08 16:42:12 -0500

    remove extra line and fix incorrect index name

--- db/migrations/20210307225524_user_locations.js
+++ db/migrations/20210307225524_user_locations.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("user_locations", t => {
   t.bigInteger("location_id").notNullable();
   t.datetime("created_at", { precision: 6 }).defaultTo(knex.fn.now(6)).notNullable();
   t.datetime("updated_at", { precision: 6 }).defaultTo(knex.fn.now(6)).notNullable();
-  t.unique(["location_id", "user_id"], "index_user_locations_on_netsuite_internal_id_and_user_id")
+  t.unique(["location_id", "user_id"], "index_user_locations_on_location_id_and_user_id")
   t.index("user_id", "index_user_locations_on_user_id")
 });
 
