859848a633a322d21a4a9ce4fc024e1f38c04325
AnthonyClausing <AnthonyClausing@gmail.com>
2021-06-10 15:02:18 -0400

    rm useless imports, rm test routes, add settings routes for testing

--- src/lib/config/app.ts
+++ src/lib/config/app.ts
@@ -1,23 +1,23 @@
 import express, { Application } from 'express'
-import { TestRoutes } from '../routes/test_routes'
 import { CommonRoutes } from '../routes/common_routes'
 import { UserRoutes } from '../routes/user_routes'
 import { UserShiftRoutes } from '../routes/shift_routes'
+import { SettingsRoutes } from '../routes/settings_routes'
 
 class App {
   public app: Application
-  private test_routes: TestRoutes = new TestRoutes()
   private common_routes: CommonRoutes = new CommonRoutes()
   private user_routes: UserRoutes = new UserRoutes()
   private shift_routes: UserShiftRoutes = new UserShiftRoutes()
-
+  private settings_routes: SettingsRoutes = new SettingsRoutes()
   constructor() {
     this.app = express()
     this.app.use(express.urlencoded({ extended: false }))
     this.app.use(express.json())
-    this.test_routes.route(this.app)
     this.user_routes.route(this.app)
     this.shift_routes.route(this.app)
+    this.settings_routes.route(this.app)
+    //common routes are last, catch all is located there
     this.common_routes.route(this.app)
   }
 }
--- src/lib/routes/shift_routes.ts
+++ src/lib/routes/shift_routes.ts
@@ -1,4 +1,4 @@
-import { Application, Request, Response } from 'express'
+import { Application } from 'express'
 import LocationUserShiftController from '../controllers/locationUserShift'
 
 export class UserShiftRoutes {
